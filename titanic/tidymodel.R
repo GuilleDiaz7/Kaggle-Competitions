d1cb8b693ea933f4a803ea2fb65f9b5222c4fd26
Guillermo Diaz <guillermo.dgr.7@gmail.com>
2022-10-03 14:50:47 +0200

    Update.

--- titanic/tidymodel.R
+++ titanic/tidymodel.R
@@ -2,6 +2,8 @@ library(tidymodels)
 library(tidyverse)
 library(ggplot2)
 
+rm(list = ls())
+
 titanic_train <- read_csv("titanic/data/train.csv") %>% 
   mutate(across(where(is.character) | where(is.logical), as.factor))
 titanic_test <- read_csv("titanic/data/test.csv") %>% 
@@ -25,17 +27,34 @@ set.seed(123)
 titanic_folds <- vfold_cv(titanic_train, strata = Survived)
 
 glm_recipe <- recipe(Survived ~., data = titanic_train) %>% 
-  update_role(PassengerId, new_role = "Id") %>% 
+  update_role(PassengerId, new_role = "Id") %>%   
+  step_rm(Ticket) %>% 
+  step_string2factor(all_nominal_predictors()) %>% 
+  step_num2factor(Pclass, levels = c("First", "Second", "Third")) %>% 
   step_mutate(Survived = factor(
     Survived,
     levels = c(0, 1),
     labels = c("Deceased", "Survived")
-  )) %>% 
-  step_string2factor(all_nominal_predictors()) %>% 
+  )
+  ) %>% 
+  step_mutate(
+    title = str_match(Name, ", ([:alpha:]+)\\."),
+    title = if_else(is.na(title[, 2]), "NA", title[, 2])
+  ) %>% 
+  step_other(title, threshold = 0.02, other = "Other") %>% 
+  update_role(Name, new_role = "Id") %>% 
+  step_mutate(Cabin = if_else(is.na(Cabin), "Missing", "Available")) %>% 
+  step_impute_knn(all_predictors()) %>% 
   step_novel(all_nominal_predictors()) %>% 
   step_dummy(all_nominal_predictors()) %>% 
   step_zv(all_predictors()) %>% 
   step_normalize(all_numeric_predictors())
+prep(glm_recipe)
+
+glm_baked <- glm_recipe %>% 
+  prep() %>% 
+  bake(new_data = NULL)
+names(glm_baked)
 
 glm_spec <- logistic_reg() %>% 
   set_engine("glm")
@@ -72,3 +91,6 @@ glm_results %>%
   geom_abline(lty = 2, color = "gray80", size = 1.5) +
   geom_path(show.legend = FALSE, alpha = 0.6, size = 1.2) +
   coord_equal()
+
+fitted <- fit(glm_wf, titanic_train)
+predict(fitted, new_data = titanic_test)
